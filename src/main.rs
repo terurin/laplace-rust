c1769d8a56ebb376d9ebde2f023555a44a8835ea
terurin <hamusutaateru@live.jp>
2019-09-09 10:49:20 +0900

    収束がおかしい

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 #![feature(test)]
 extern crate rayon;
 extern crate test;
-use std::sync::{mpsc, Arc, Mutex};
+extern crate threadpool;
+use std::sync::{mpsc, Arc,Mutex,RwLock};
 use std::thread;
-const size: usize = 8;
+use threadpool::ThreadPool;
+const ncpus:usize=2;
+const size: usize = 64;
 
 fn main() {
-    parallel();
+    single();
+    //parallel();
 }
 
 fn single() {
@@ -36,7 +40,7 @@ fn single() {
         }
 
         count += 1;
-        if (count % 100) == 0 {
+        if (count % 10) == 0 {
             let mut sum = 0.0;
             for i in 1..size + 1 {
                 for j in 1..size + 1 {
@@ -54,81 +58,92 @@ fn single() {
 
 fn parallel() {
     let (dx, dy) = (1. / size as f32, 1. / size as f32);
-    let tile = dx * dy;
     let heat = 10.0;
     let eps = 0.001;
-    let mut now: Vec<Arc<Mutex<Vec<f32>>>> = Vec::new();
-    let mut last_tmp: Vec<Vec<f32>> = Vec::new();
+    let mut now: Vec<Arc<RwLock<Vec<f32>>>> = Vec::new();
+    let mut last: Vec<Arc<RwLock<Vec<f32>>>> = Vec::new();
     for _ in 0..size + 2 {
-        now.push(Arc::new(Mutex::new(vec![0.0; size + 2])));
-        last_tmp.push(vec![0.0; size + 2]);
+        now.push(Arc::new(RwLock::new(vec![0.0; size + 2])));
+        last.push(Arc::new(RwLock::new(vec![0.0; size + 2])));
     }
-    let mut last: Arc<Vec<Vec<f32>>> = Arc::new(last_tmp);
+    
     let mut count = 0;
+    let pool = ThreadPool::new(ncpus);
     loop {
+        //last <- now
+        {
+            let (tx, rx) = mpsc::channel();
+            for i in 1 .. size+1{
+                let last_row=last[i].clone();
+                let now_row=now[i].clone();
+                let tx=tx.clone();
+                pool.execute(move||{
+                    let mut last_row=last_row.write().unwrap();
+                    let now_row=now_row.read().unwrap();
+                    for j in 1 .. size+1{
+                        last_row[j]=now_row[i];
+                    }
+                    tx.send(()).unwrap();
+                });
+            }
+            for _ in 1 ..size+1{
+                rx.recv().unwrap();
+            }
+        }
+        
         //next step now <- last
         {
             //heat simulater
             let (tx, rx) = mpsc::channel();
             for i in 1..size + 1 {
-                let now_row = now[i].clone();
-                let last = last.clone();
-                let tx = tx.clone();
-                thread::spawn(move || {
-                    let mut now_row = now_row.lock().unwrap();
-                    for j in 1..size + 1 {
-                        (*now_row)[j] = (heat * tile
-                            + last[i + 1][j + 0]
-                            + last[i - 1][j + 0]
-                            + last[i + 0][j + 1]
-                            + last[i + 0][j - 1])
-                            / 4.0;
+                let last_up=last[i-1].clone();
+                let last_mid=last[i].clone();
+                let last_down=last[i+1].clone();
+                let now_row=now[i].clone();
+                let tx=tx.clone();
+                pool.execute(move||{
+                    let last_up=last_up.read().unwrap();
+                    let last_mid=last_mid.read().unwrap();
+                    let last_down=last_down.read().unwrap();
+                    let mut now_row=now_row.write().unwrap();
+                    for j in 1 ..size+1 {
+                        now_row[j]=(heat*dx*dy+last_up[j]+last_mid[j-1]+last_mid[j]+last_mid[j+1])/4.0;
                     }
                     tx.send(()).unwrap();
                 });
             }
-            //join session
-            for _ in 1..size + 1 {
+            
+            for _ in 1 ..size+1{
                 rx.recv().unwrap();
             }
         }
-        {
-            count += 1;
-            if (count%100)==0{
-                let (tx, rx) = mpsc::channel();
-                for i in 1..size + 1 {
-                    let now_row = now[i].clone();
-                    let last = last.clone();
-                    let tx = tx.clone();
-                    thread::spawn(move || {
-                        let now_row = now_row.lock().unwrap();
-                        let mut sum = 0.0;
-                        for j in 1..size + 1 {
-                            let d = last[i][j] - (*now_row)[j];
-                            sum += (d * d).sqrt();
-                        }
-                        tx.send(sum).unwrap();
-                    });
-                }
-                let mut sum = 0.0;
-                for _ in 1..size + 1 {
-                    sum += rx.recv().unwrap();
-                }
-                println!("Count={},Error={}", count, sum);
-                if sum < eps {
-                    break;
-                }
-            }
-        }
-        //copy session
-        {
-            let mut last_tmp: Vec<Vec<f32>> = Vec::new();
-            for i in 0.. size+2{
+        
+        count+=1;
+        if (count %10)==0{
+            let (tx, rx) = mpsc::channel();
+            for i in 1 .. size+1{
+                let last_row=last[i].clone();
                 let now_row=now[i].clone();
-                let now_row=now_row.lock().unwrap();
-                last_tmp.push(now_row.to_vec());
+                let tx=tx.clone();
+                pool.execute(move||{
+                    let last_row=last_row.read().unwrap();
+                    let now_row=now_row.read().unwrap();
+                    let mut error=0.0;
+                    for j in 1 .. size+1{
+                        let d=last_row[j]-now_row[i];
+                        error+=(d*d).sqrt();
+                    }
+                    tx.send(error).unwrap();
+                });
             }
-            last=Arc::new(last_tmp);
+            let mut error=0.0;
+            for _ in 0..size{
+                error+=rx.recv().unwrap();
+            }
+            println!("Count={},Error={}", count, error);
+            if error<eps{
+                break;
+            }
         }
     }
 }
